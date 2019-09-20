2f6eb15a6dea6e0c48bd3f1edab1bed8b38ea5df
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-20 04:36:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,9 +18,9 @@ class Song
     new_song = Song.new(song)
     new_song.artist_name = artist
     new_song
+  end
 
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)  
   end
 end
