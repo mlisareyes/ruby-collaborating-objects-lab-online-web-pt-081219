cdafc49fbb2e05a756a5874f2312ea4f52b070e5
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-20 04:26:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    song = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    new_song = Song.new(song)
+    new_song.artist = artist
+    new_song
+
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+    end
+  end
+end
