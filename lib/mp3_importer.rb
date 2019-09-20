385449087a4e1131883dd5cb29f947e7fe5d46c7
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-20 04:34:28 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,5 +6,14 @@ class MP3Importer
   end
 
   def files
+    files = []
+    Dir.new(path).each do |file|
+      files << file if file.end_with?('.mp3')
+    end
+    files
+  end
+
+  def import
+    self.files.each { |file| Song.new_by_filename(file) }
   end
 end
